04099cc2bccd67578691cabdb26cd75aa1af376c
sintak <sintak@126.com>
2017-07-03 16:27:21 +0800

    es6 module test

---
+++ htmlcss/js/es6/module/demo1.js
@@ -0,0 +1,7 @@
+export const str = 'hello world'
+
+export function f(a){
+    return a+1
+}
+
+export default const str1 = 'hello world'
---
+++ htmlcss/js/es6/module/demo2.js
@@ -0,0 +1,4 @@
+import { str, f } from 'demo1' //也可以分开写两次，导入的时候带花括号
+
+
+import s1 from 'demo1'
