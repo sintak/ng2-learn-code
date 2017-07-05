4f61e4a5b88ade7113049b7bb3c76713f62e631f
sintak <sintak@126.com>
2017-07-05 09:20:48 +0800

    event test

---
+++ htmlcss/js/event/app.js
@@ -0,0 +1,28 @@
+Event.addEvent("alert", function() {
+    alert("弹出！");
+});
+
+// 触发自定义alert事件
+Event.fireEvent("alert");
+
+// ----------------------
+
+// 点击页面任意空白区域。
+var myEvents = new EventTarget();
+myEvents.addEvents({
+    "once": function() {
+        alert("该弹框只会出现一次！");    
+        this.removeEvent("once");
+    },
+    "infinity": function() {
+        alert("每次点击页面，该弹框都会出现！");    
+    }
+});
+
+document.onclick = function(e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement;
+    if (!target || !/input|pre/i.test(target.tagName)) {
+        myEvents.fireEvents(["once", "infinity"]);
+    }
+};
